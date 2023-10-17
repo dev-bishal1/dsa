4ede3a4970fe2b922567110459fce7f770d3073b
Bishal Rayamajhi <rayamajhibishal556@gmail.com>
2023-10-17 07:31:14 +0545

    isValidSudoku problem solved

---
+++ ArrayAndHashing/isValidSudoku.js
@@ -0,0 +1,55 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ * 
+ * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+ */
+
+const isValidSudoku = (board) => {
+  let rows = Array.from({ length: 9 }, () => new Set());
+  let cols = Array.from({ length: 9 }, () => new Set());
+  let boxes = Array.from({ length: 9 }, () => new Set());
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      let num = board[i][j];
+      if (num !== ".") {
+        let boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+
+        if (rows[i].has(num) || cols[j].has(num) || boxes[boxIndex].has(num)) {
+          return false;
+        }
+
+        rows[i].add(num);
+        cols[j].add(num);
+        boxes[boxIndex].add(num);
+      }
+    }
+  }
+
+  return true;
+};
+
+let board = [
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+];
+
+console.log(isValidSudoku(board));
